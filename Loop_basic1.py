bb3439d6a19f61afe335a1495600aa3fea96a1ec
Laksh Singh <132688007+LakshSingh1@users.noreply.github.com>
2023-06-07 13:08:57 -0700

    Add files via upload

---
+++ Loop_basic1.py
@@ -0,0 +1,31 @@
+for n in range(1,150):
+    print(n)
+
+
+for i in range (5,1000):
+    if i%5==0:
+        print(i)
+
+
+for x in range (1,100):
+    print(x)
+
+    if x % 10 == 0:
+        print("Coding Dojo")
+
+    elif x %5==0:
+        print("Coding")
+
+
+sum = 0
+for i in range(1, 500001, 2):
+    sum += i
+
+print(sum)
+
+
+num = 2018
+
+while num > 0:
+    print(num)
+    num -= 4
